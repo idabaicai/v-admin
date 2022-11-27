5a1146b78b543ae0be2d274a7db934c75e8ba08b
idabaicai <icandebug@gmail.com>
2022-11-27 20:42:20 +0800

    chore: init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import type { Ref } from 'vue'
+import Upload from './components/v-upload/VUpload.vue'
+const name: Ref<string> = ref('hhh')
+
+interface Res {
+  successful: Boolean
+  resultValue: any
+}
+const res: Ref<Res> = ref({ successful: true, resultValue: [] })
+
+onMounted(() => {
+  console.log(name)
+  const arr = [1, 2, 3, 0]
+  const hasLogin = 3 / 2 === 1
+  arr.forEach((item) => {
+    console.log(item)
+  })
+})
+</script>
+
+<template>
+  <div> vue3 </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/v-upload/VUpload.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    vue + vite + ts admin
+  </div>
+</template>
+
+<style lang="less" scoped>
+</style>
